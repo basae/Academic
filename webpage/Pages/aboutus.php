f17727932f3dd3b3ee2cae88d10d0c32c0d733bf
ferchoCrown <sastrelz_fer@hotmail.com>
2014-04-14 15:11:49 -0500

    imagen en mision y vision

--- webpage/Pages/aboutus.php
+++ webpage/Pages/aboutus.php
@@ -1,5 +1,9 @@
 <div class="row-fluid">
 	<div class="col-lg-12">
+	      <p align="center">
+		    <img src="Content/images/somos.jpg" height="147" width="1000"/>
+			<img src="Content/images/somos2.png" height="260" width="1050" />
+          </p>
     	<h3>Misión</h3>
         <p class="text-justify">
         	Somos una empresa sofisticada y vanguardista en el ámbito tecnológico e informático, que contribuye a la educación mediante el uso de la tecnología de primera mano, generando un valor agregado y significativo al desarrollo académico de nuestro país convirtiendo a esta en educación de calidad.
@@ -8,5 +12,9 @@
         <p class="text-justify">
         	Ser una empresa reconocida, distinguida y renombrada en el ámbito estudiantil debido a la transformación de métodos de enseñanza tradicionales, por los medios de aprendizaje interactivos y tecnológicos que proponemos, creado educación de calidad y contribuyendo a la educación de este país y en un futuro a un mundo globalizado.
         </p>
+		 
     </div>
+	
+
+	
 </div>
